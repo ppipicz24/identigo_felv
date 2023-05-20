a01e5d86591faf2d337e71916b27834f679c140b
Pipicz Petra <ppipicz24@gmail.com>
2023-05-20 15:59:43 +0200

    adatbázis

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Company;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,6 +14,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+        $users = collect();
+        $companies = collect();
+
+        $users->add(User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.hu',
+            'password' => 'password'
+        ]));
+
+        $companyCount = rand(10,15);
+
+        for ($i=0; $i < $companyCount; $i++) { 
+            $companies->add(Company::factory()->create([
+                'user_id' => $users->random()->id
+            ]));
+        }
+
+
 
 
     }
